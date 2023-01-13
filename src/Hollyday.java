873fdd6391e24afa9f566475a387e1cbc742779d
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-13 13:16:48 -0300

    feat: create hollyday class

---
+++ src/Hollyday.java
@@ -0,0 +1,17 @@
+public class Hollyday {
+    private String date;
+    private String description;
+
+    public Hollyday(String date, String description) {
+        this.date = date;
+        this.description = description;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
